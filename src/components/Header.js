43a0b59f64c3de3e0683efa2de09a6ce7bbdab5f
samirvithlani <samir.vithlani83955@gmail.com>
2022-11-07 14:53:24 +0530

    initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Header = () => {
+  var name = "raj";
+  var age = 25;
+  var isMarried = true;
+  var salary = 12345.6;
+  var style = {
+    color: "red",
+    fontSize: "20px",
+  };
+  return (
+    <div>
+      <h1 className="header"></h1>
+      <h1>Name = {name}</h1>
+      <h2>age = {age}</h2>
+      <h3 style={style}>isMarried = {isMarried ? "YES" : "NO"}</h3>
+      <h4>salary = {salary}</h4>
+    </div>
+  );
+};
